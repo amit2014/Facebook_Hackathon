1b0206445e6a65642e25796f0dcc36e8377b52ab
Jeremy Kao <j7kao@ucsd.edu>
2013-04-13 08:21:55 -0700

    Reorganizing files

--- fb_functions.js
+++ fb_functions.js
@@ -1,36 +1,47 @@
 userArray = {};
+randomNums = new Array(26);
+randomNumSeen = new Array(52);
 
 	$("#fb_login").click(login);
     function login() {
     	FB.login(function(response) {
         	if (response.authResponse) {
             // connected
-            testAPI();
+            getUsers();
+            console.log("Got newsfeed!");
         	} else {
             // cancelled
             console.log("stuff dont even work");
       	  }
     	},{scope: 'read_stream'});
 		}
-    function testAPI() {
- 			FB.api('/me/home', function(response) {
-  	      console.log( response );
-    	});
-		}
 
 function getUsers(){
-	FB.api('/me/home', function(response){
-		for(var i = 0; i < response['data'].length; i++){
-      var username = response['data'][i]['from']['name'];
-      if (userArray[username])
-        userArray[username][0] += 1;
-      else
-        userArray[username] = [1,0];
+	FB.api('/me/home?limit=100', function(response){
+	for(var i = 0; i < response['data'].length; i++){
+    var username = response['data'][i]['from']['name'];
+    if (userArray[username])
+      userArray[username][0] += 1;
+    else
+      userArray[username] = [1,0];
+	 }
+   });
+
+  /* creating object of non-repeating random nums */
+    var randomNumNote = 0;
+    for( var i = 0; i < randomNumSeen.length; i++ ){
+	   	randomNumNote = Math.floor(Math.random()*55);
+      randomNumSeen.push(randomNumNote);
     }
-    for ( var name in userArray ){
-      console.log(name + " " + userArray[name][1]);
-    }
-	});
+
+    for ( var names in userArray ){
+      userArray[name][0] = randomNumSeen ;
+      console.log(name + " " + userArray[name][0]);
+ 		}
+
 }
 
+function getPosts(){
+  /* fucken piece of shit mother fuckers of a dick head */
+} 
 $("#getUsersButton").click(getUsers);
